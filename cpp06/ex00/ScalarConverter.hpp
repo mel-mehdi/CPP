e254318dd86eb7c542c0860d7c603664c82bbda8
melmehdi <melmehdi@e1r9p9.1337.ma>
2025-05-09 09:55:01 +0100

    cpp06

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -1,40 +1,33 @@
-#ifndef SCALAR_CONVERTER_HPP
-#define SCALAR_CONVERTER_HPP
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
 #include <limits>
 #include <cmath>
-#include <cctype>
 #include <string>
-#include <iostream>
-#include <sstream>
-#include <iomanip>
-#include <climits>
-
+#include <cerrno>
 
 class ScalarConverter {
-private:
-    ScalarConverter();
-    ScalarConverter(const ScalarConverter& other);
-    ScalarConverter& operator=(const ScalarConverter& other);
-    ~ScalarConverter();
+public:
+    static void convert(const std::string& literal);
 
+private:
     static bool isChar(const std::string& literal);
     static bool isInt(const std::string& literal);
     static bool isFloat(const std::string& literal);
     static bool isDouble(const std::string& literal);
-    
-    static void convertFromChar(char c);
-    static void convertFromInt(int i);
-    static void convertFromFloat(float f);
-    static void convertFromDouble(double d);
 
-    static void printChar(double value);
-    static void printInt(double value);
-    static void printFloat(double value);
-    static void printDouble(double value);
+    static void printChar(char c);
+    static void printInt(int i);
+    static void printFloat(float f);
+    static void printDouble(double d);
 
-public:
-    static void convert(const std::string& literal);
+    ScalarConverter();
+    ScalarConverter(const ScalarConverter& other);
+    ScalarConverter& operator=(const ScalarConverter& other);
+    ~ScalarConverter();
 };
 
 #endif
